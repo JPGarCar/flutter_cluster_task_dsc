3adf54892c61ec177dbedd4311982824f8a43722
Juan Pablo Garcia <juanpablo.garcia@alumni.ubc.ca>
2020-09-10 17:51:28 -0500

    Final changes to design, also added constants.dart file.

---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+/// All the constants used in multiple files of the app are stored here
+
+final mainColor = Colors.deepPurpleAccent.shade200;
--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_cluster_task/screens/about_screen.dart';
 import 'package:flutter_cluster_task/screens/home_screen.dart';
 
+import '../constants.dart';
+
+/// Drawer to be used across the entire application, it holds a drawer header,
+/// a list of items, and at the very bottom a container with a text
+
 // Drawer title, color, and text style
 final drawerTitle = 'Demo app DSC';
 final drawerTitleStyle = TextStyle(
@@ -9,42 +14,63 @@ final drawerTitleStyle = TextStyle(
   fontSize: 24,
 );
 
-final drawerTitleColor = Colors.deepPurpleAccent.shade200;
-
 class AppDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          // Header
-          DrawerHeader(
-            decoration: BoxDecoration(color: drawerTitleColor),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              crossAxisAlignment: CrossAxisAlignment.start,
+          Flexible(
+            child: ListView(
+              padding: EdgeInsets.zero,
               children: [
-                Text(
-                  drawerTitle,
-                  style: drawerTitleStyle,
+                // Header
+                DrawerHeader(
+                  decoration: BoxDecoration(color: mainColor),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        drawerTitle,
+                        style: drawerTitleStyle,
+                      ),
+                    ],
+                  ),
+                ),
+                // List of Children Buttons
+                ListTile(
+                  leading: Icon(Icons.home),
+                  title: Text('Home'),
+                  onTap: () =>
+                      Navigator.popAndPushNamed(context, HomeScreen.screenId),
+                ),
+                ListTile(
+                  leading: Icon(Icons.info_outline),
+                  title: Text('About'),
+                  onTap: () =>
+                      Navigator.popAndPushNamed(context, AboutScreen.screenId),
                 ),
               ],
             ),
           ),
-          // List of Children Buttons
-          ListTile(
-            leading: Icon(Icons.home),
-            title: Text('Home'),
-            onTap: () =>
-                Navigator.popAndPushNamed(context, HomeScreen.screenId),
-          ),
-          ListTile(
-            leading: Icon(Icons.info_outline),
-            title: Text('About'),
-            onTap: () =>
-                Navigator.popAndPushNamed(context, AboutScreen.screenId),
-          ),
+          Container(
+            color: mainColor,
+            child: Center(
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 5),
+                child: Text(
+                  'Powered by google developers',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 12,
+                  ),
+                ),
+              ),
+            ),
+          )
         ],
       ),
     );
